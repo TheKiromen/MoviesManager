54817a636638846a0b4cc11bb527a30933284d3c
TheKiromen <TheKiromen@gmail.com>
2022-03-27 20:12:23 +0200

    Initial commit

---
+++ src/main/java/com/dkrucze/MoviesManager/MoviesManagerApplication.java
@@ -0,0 +1,13 @@
+package com.dkrucze.MoviesManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesManagerApplication.class, args);
+	}
+
+}
